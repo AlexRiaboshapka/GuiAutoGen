b9594e8c986d2efd23f30689f298c608d8d0738c
AlexRiaboshapka <alex.riaboshapka@gmail.com>
2022-12-18 18:24:15 +0100

    Fix test to be more resilient

---
+++ src/test/java/ui/cita/checkCitaIsAvailable.java
@@ -0,0 +1,44 @@
+package ui.cita;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import ui.common.AbstractBaseTest;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+import static helper.CsvHelper.parseCsv;
+
+public class checkCitaIsAvailable extends AbstractBaseTest {
+    public final String url = "https://icp.administracionelectronica.gob.es/icpco/citar?p=3&locale=es";
+    private final String csvPath = "src/test/resources/datafiles/users.csv";
+
+    @BeforeClass
+    public void setUp() {
+        open(url);
+    }
+
+    @DataProvider
+    public Iterator<Object[]> users() throws IOException {
+        return parseCsv(csvPath);
+    }
+
+    @Test(dataProvider = "users")
+    public void checkCitaIsAvailable(String nie, String name) {
+        citaPrevia.selectCitaTypeAndAccept();
+        citaPrevia.clickEnterButton();
+        citaPrevia.inputNieAndClickAccept(nie, name);
+        citaPrevia.clickEnterButton2();
+        citaPrevia.getCitaInfo();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        closeWebDriver();
+    }
+}
+
